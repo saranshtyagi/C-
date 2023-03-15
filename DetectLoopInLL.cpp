f430e72f01cf961c16f053b638620a432ba6bce9
saranshtyagi <41801980+saranshtyagi@users.noreply.github.com>
2023-03-15 13:26:00 +0530

    Create DetectLoopInLL.cpp

---
+++ DetectLoopInLL.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        if(head==NULL){
+            return false;
+        }
+        map<Node*, bool>visited;
+        
+        Node* temp=head;
+        
+        while(temp!=NULL){
+            
+            //cycle is present
+            if(visited[temp]==true){
+                return true;
+            }
+            visited[temp]=true;
+            temp=temp->next;
+        }
+        return false;
+    }
+};
