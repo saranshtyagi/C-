38fef900e3efe4e19ba05cd28bc501b329bf2e66
saranshtyagi <41801980+saranshtyagi@users.noreply.github.com>
2023-03-30 15:36:30 +0530

    Create Sort0s1s2sInLL.cpp

---
+++ Sort0s1s2sInLL.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        int zeroCount=0;
+        int oneCount=0;
+        int twoCount=0;
+        Node* temp=head;
+        
+        while(temp!=NULL){
+            if(temp->data==0){
+                zeroCount++;
+            }
+            else if(temp->data==1){
+                oneCount++;
+            }
+            else if(temp->data==2){
+                twoCount++;
+            }
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            if(zeroCount!=0){
+                temp->data=0;
+                zeroCount--;
+            }
+            else if(oneCount!=0){
+                temp->data=1;
+                oneCount--;
+            }
+            else if(twoCount!=0){
+                temp->data=2;
+                twoCount--;
+            }
+            temp=temp->next;
+        }
+        return head;
+    }
+};
