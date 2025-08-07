4c85624aecf4d9e7b7377e29d7cfe476c783a510
Saransh Tyagi <41801980+saranshtyagi@users.noreply.github.com>
2025-08-07 22:31:38 +0530

    Time: 64 ms (62.99%), Space: 156.2 MB (55.84%) - LeetHub

---
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int n = fruits.size();
+        vector<vector<int>> t(n, vector<int>(n, 0));
+        int result = 0;
+
+        for(int i = 0; i < n; i++) {
+            result += fruits[i][i];
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i < j && i+j < n-1) {
+                    t[i][j] = 0;
+                } else if(i > j && i+j < n-1) {
+                    t[i][j] = 0;
+                } else {
+                    t[i][j] = fruits[i][j];
+                }
+            }
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                t[i][j] += max({t[i-1][j-1], t[i-1][j], (j+1 < n) ? t[i-1][j+1] : 0});
+            }
+        }
+
+        for(int j = 1; j < n; j++) {
+            for(int i = j+1; i < n; i++) {
+                t[i][j] += max({t[i-1][j-1], t[i][j-1], (i+1 < n) ? t[i+1][j-1] : 0});
+            }
+        }
+        return result + t[n-2][n-1] + t[n-1][n-2];
+
+    }
+};
