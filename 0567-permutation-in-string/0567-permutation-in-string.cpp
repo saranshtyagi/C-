e52cdfdc09e8e13d557bf50ffd1f81867c34f460
Saransh Tyagi <41801980+saranshtyagi@users.noreply.github.com>
2026-01-31 23:42:31 +0530

    Time: 11 ms (34.36%), Space: 10.9 MB (22.13%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        sort(s1.begin(), s1.end()); 
-
         int n = s1.size(); 
-        int m = s2.size(); 
+        int m  = s2.size(); 
 
-        if(n > m) return false; 
+        unordered_map<char, int> freqS1; 
+        unordered_map<char, int> freqS2; 
 
-        for(int i = 0; i <= m - n; i++) {
-            string substring = s2.substr(i, n); 
-            sort(substring.begin(), substring.end()); 
+        for(int i = 0; i < n; i++) {
+            freqS1[s1[i]]++;
+        }
 
-            if(s1 == substring) {
+        for(int i = 0; i < m; i++){
+            freqS2[s2[i]]++;
+
+            if(i >= n) {
+                freqS2[s2[i - n]]--;
+                if(freqS2[s2[i - n]] == 0) {
+                    freqS2.erase(s2[i - n]);
+                }
+            }
+
+            if(i >= n - 1 && freqS1 == freqS2) {
                 return true;
             }
         }
