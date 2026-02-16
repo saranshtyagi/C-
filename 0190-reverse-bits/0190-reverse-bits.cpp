f4fcbcb162681dac7b81bc640e984539260b6640
Saransh Tyagi <41801980+saranshtyagi@users.noreply.github.com>
2026-02-16 10:17:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (73.18%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        int result = 0; 
+        for(int i = 1; i <= 32; i++) {
+            result = result << 1; 
+            result = (result | (n & 1));
+            n = n >> 1;
+        }
+        return result;
+    }
+};
