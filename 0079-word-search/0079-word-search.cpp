82eb1ba316a19908504b25a6d3b6d73e924e40b7
Saransh Tyagi <41801980+saranshtyagi@users.noreply.github.com>
2026-07-08 16:22:40 +0530

    Time: 915 ms (8.28%), Space: 11.7 MB (30.26%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,34 +1,36 @@
 class Solution {
-private:
-    bool found(vector<vector<char>>& board, int i, int j, string& word, int idx) {
+public:
+    int rows, cols;
+    bool find(vector<vector<char>>& board, int i, int j, string word, int idx) {
         if(idx == word.length()) {
             return true;
         }
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == '$') {
+        if(i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] == '$') {
             return false;
         }
-        if(board[i][j] != word[idx]) {
-            return false;
+        if(word[idx] != board[i][j]) {
+            return false; 
         }
+
         char temp = board[i][j]; 
         board[i][j] = '$'; 
-        bool find = found(board, i - 1, j, word, idx + 1) ||
-                     found(board, i + 1, j, word, idx + 1) ||
-                     found(board, i, j - 1, word, idx + 1) ||
-                     found(board, i, j + 1, word, idx + 1); 
-        board[i][j] = temp;
-        return find;
-    }
-public:
-    bool exist(vector<vector<char>>& board, string word) {
-        int row = board.size(); 
-        int col = board[0].size(); 
 
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if((board[i][j] == word[0]) && (found(board, i, j, word, 0))) {
+        bool found = find(board, i + 1, j, word, idx + 1) | 
+                     find(board, i - 1, j, word, idx + 1) | 
+                     find(board, i, j + 1, word, idx + 1) | 
+                     find(board, i, j - 1, word, idx + 1); 
+        board[i][j] = temp; 
+        return found;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        rows = board.size(); 
+        cols = board[0].size(); 
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(board[i][j] == word[0] && find(board, i, j, word, 0)) {
                     return true;
-                } 
+                }
             }
         }
         return false;
